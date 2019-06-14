b747085ad690551837ae922d778770a325a7121f
malaba <malabatalent@gmail.com>
2019-06-14 16:31:11 +0800

    added axios and able to fetch images from API
    added axios and able to fetch images from API

---
+++ src/components/navbar/navbar.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import AppBar from 'material-ui/AppBar';
+
+export default function Navbar(props) {
+
+    return (
+        <AppBar title="Material-UI Pixabay App">
+
+        </AppBar>
+    )
+}
---
+++ src/components/search/search.js
@@ -0,0 +1,66 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+import TextField from 'material-ui/TextField';
+import SelectField from 'material-ui/SelectField';
+import MenuItem from 'material-ui/MenuItem';
+
+const corsURL = `https://cors-anywhere.herokuapp.com/`;
+const API_key = process.env.REACT_APP_PIXABAY_API_KEY;
+const API_URL = `${ corsURL }https://pixabay.com/api/?key=${ API_key }`;
+
+
+export default class Search extends Component {
+
+    state = {
+        searchTerm: '',
+        amount: 10,
+        images: []
+    };
+
+    onTextInput = async e => {
+        try {
+            this.setState({ [e.target.name]: e.target.value });
+            let res = await axios.get(`${ API_URL }&q=${ this.state.searchTerm }&image_type=photo&per_page=${ this.state.amount }`);
+            // console.log(res.data);
+            this.setState({ images: res.data.hits });
+        } catch (e) {
+            console.log(e);
+        }
+    };
+
+    onAmountChange = e => {
+        this.setState({ [e.target.name]: e.target.value });
+    };
+
+    render() {
+
+        console.log(this.state.images);
+
+        return (
+            <div>
+                <TextField
+                    name="searchTerm"
+                    value={ this.state.searchTerm }
+                    floatingLabelText="search for images"
+                    fullWidth={ true }
+                    onChange={ this.onTextInput }>
+                </TextField>
+                <br />
+                <SelectField
+                    name="amount"
+                    floatingLabelText="Amount"
+                    value={ this.state.amount }
+                    onChange={this.onAmountChange}
+                >
+                    <MenuItem value={5} primaryText="5" />
+                    <MenuItem value={10} primaryText="10" />
+                    <MenuItem value={15} primaryText="15" />
+                    <MenuItem value={30} primaryText="30" />
+                    <MenuItem value={50} primaryText="50" />
+                </SelectField>
+                <br />
+
+            </div>
+        )
+    }
+};
